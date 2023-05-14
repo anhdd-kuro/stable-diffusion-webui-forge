anhdd-kuro/stable-diffusion-webui-forge

commit b0280c68078da7e14532a4dfc290650872982702

Get rid of check_run + run_python

launch.py
@@ -103,11 +103,6 @@ def run(command, desc=None, errdesc=None, custom_env=None, live: bool = default_
     return (result.stdout or "")
 
 
-def check_run(command):
-    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    return result.returncode == 0
-
-
 def is_installed(package):
     try:
         spec = importlib.util.find_spec(package)
@@ -121,10 +116,6 @@ def repo_dir(name):
     return os.path.join(script_path, dir_repos, name)
 
 
-def run_python(code, desc=None, errdesc=None):
-    return run(f'"{python}" -c "{code}"', desc, errdesc)
-
-
 def run_pip(command, desc=None, live=default_command_live):
     if args.skip_install:
         return
@@ -133,8 +124,9 @@ def run_pip(command, desc=None, live=default_command_live):
     return run(f'"{python}" -m pip {command} --prefer-binary{index_url_line}', desc=f"Installing {desc}", errdesc=f"Couldn't install {desc}", live=live)
 
 
-def check_run_python(code):
-    return check_run(f'"{python}" -c "{code}"')
+def check_run_python(code: str) -> bool:
+    result = subprocess.run([python, "-c", code], capture_output=True, shell=True)
+    return result.returncode == 0
 
 
 def git_clone(url, dir, name, commithash=None):
@@ -261,8 +253,11 @@ def prepare_environment():
     if args.reinstall_torch or not is_installed("torch") or not is_installed("torchvision"):
         run(f'"{python}" -m {torch_command}', "Installing torch and torchvision", "Couldn't install torch", live=True)
 
-    if not args.skip_torch_cuda_test:
-        run_python("import torch; assert torch.cuda.is_available(), 'Torch is not able to use GPU; add --skip-torch-cuda-test to COMMANDLINE_ARGS variable to disable this check'")
+    if not args.skip_torch_cuda_test and not check_run_python("import torch; assert torch.cuda.is_available()"):
+        raise RuntimeError(
+            'Torch is not able to use GPU; '
+            'add --skip-torch-cuda-test to COMMANDLINE_ARGS variable to disable this check'
+        )
 
     if not is_installed("gfpgan"):
         run_pip(f"install {gfpgan_package}", "gfpgan")

commit 59589bfc07827d4405a77a63372d9c4c6b2f18e1

launch.py: Don't involve shell for running Python or Git for output
Fixes Linux regression in b0280c68078da7e14532a4dfc290650872982702

launch.py
@@ -58,7 +58,7 @@ Use --skip-python-version-check to suppress this warning.
 @lru_cache()
 def commit_hash():
     try:
-        return subprocess.check_output(f"{git} rev-parse HEAD", encoding='utf8').strip()
+        return subprocess.check_output([git, "rev-parse", "HEAD"], shell=False, encoding='utf8').strip()
     except Exception:
         return "<none>"
 
@@ -66,7 +66,7 @@ def commit_hash():
 @lru_cache()
 def git_tag():
     try:
-        return subprocess.check_output(f"{git} describe --tags", encoding='utf8').strip()
+        return subprocess.check_output([git, "describe", "--tags"], shell=False, encoding='utf8').strip()
     except Exception:
         return "<none>"
 
@@ -125,7 +125,7 @@ def run_pip(command, desc=None, live=default_command_live):
 
 
 def check_run_python(code: str) -> bool:
-    result = subprocess.run([python, "-c", code], capture_output=True, shell=True)
+    result = subprocess.run([python, "-c", code], capture_output=True, shell=False)
     return result.returncode == 0
 
 